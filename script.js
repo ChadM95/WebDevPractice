f04d12fea5b152b213fef89046f43d7572b97425
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-28 20:03:20 +0000

    add some javascript

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 document.write("<h1>JAVASCRIPT IS HERE BABY</h1");
+function MyFunc() {
+                   document.getElementById("changingP").innerHTML="New Paragraph Text BABY";
+                  }
